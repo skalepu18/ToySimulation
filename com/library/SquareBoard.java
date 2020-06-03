760263e73c8c617a11c597b299f1dc8e4368bbe6
unknown <srikanth.kalepu@gmail.com>
2020-06-03 15:50:19 +1000

    Added SquareBoard.java

---
+++ com/library/SquareBoard.java
@@ -0,0 +1,14 @@
+package com.library;
+
+public class SquareBoard  {
+
+    int rows;
+    int columns;
+
+    public SquareBoard(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+  
+}
